753c8edd874ada554b6a44c07c03fce23a090afb
Sabick-A <sabicksabi@gmail.com>
2025-01-28 00:24:35 +0530

    game topBar done

--- src/components/Map/Navbar.jsx
+++ src/components/Map/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import HomeBtn from './HomeBtn'
-import InfoBtn from './InfoBtn'
-import ProgressBar from './ProgressBar'
+import React from "react";
+import HomeBtn from "./HomeBtn";
+import InfoBtn from "./InfoBtn";
+import ProgressBar from "./ProgressBar";
+import logo2 from "../../assets/images/common/logo2.png";
 
 function Navbar() {
   return (
     <>
-        <HomeBtn/>
-        <ProgressBar progress={40}/>
-        <InfoBtn/>
+      <div className="flex justify-center">
+        <span className="absolute top-10 left-10">
+          <img src={logo2} className="h-10" alt="ConxQuest Logo" />
+        </span>
+        <ProgressBar progress={40} />
+        <InfoBtn />
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/common/Alert.jsx
+++ src/components/common/Alert.jsx
@@ -5,12 +5,12 @@ function Alert({setVisible}) {
     <div className="z-50 h-screen w-full flex items-center justify-center bg-whitesmoke opacity-90">
       <div
         id="alert-additional-content-2"
-        class="p-4 mb-4 mx-10 text-red-800 border border-red-300 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400 dark:border-red-800 z-200"
+        className="p-4 mb-4 mx-10 text-red-800 border border-red-300 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400 dark:border-red-800 z-200"
         role="alert"
       >
-        <div class="flex items-center">
+        <div className="flex items-center">
           <svg
-            class="flex-shrink-0 w-4 h-4 me-2"
+            className="flex-shrink-0 w-4 h-4 me-2"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -18,21 +18,21 @@ function Alert({setVisible}) {
           >
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
           </svg>
-          <span class="sr-only">Info</span>
-          <h3 class="text-lg font-medium">
+          <span className="sr-only">Info</span>
+          <h3 className="text-lg font-medium">
             Thank you for trying our game! 🎮{" "}
           </h3>
         </div>
-        <div class="mt-2 mb-4 text-sm font-bold">
+        <div className="mt-2 mb-4 text-sm font-bold">
           Unfortunately, we’re not supported on mobile yet, but we’re eagerly
           working on it. In the meantime, please give it a try on desktop. We
           appreciate your patience! 😊
         </div>
-        <div class="flex">
+        <div className="flex">
           <button
             onClick={() => setVisible(false)}
             type="button"
-            class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
+            className="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
             data-dismiss-target="#alert-additional-content-2"
             aria-label="Close"
           >
